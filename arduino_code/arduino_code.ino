88f1a59c02a4d1073376d40d1863e6ea1e2f34b3
Amit Barman <amitbarrman15fifteen@gmail.com>
2022-10-14 18:32:17 +0530

    Initial commit
    start to program

---
+++ arduino_code/arduino_code.ino
@@ -0,0 +1,29 @@
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% THIS IS VOYAGER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//      
+//      IT IS A TEAM OF TWO ENGINEERING STUDENTS CREATED FOR MAKING A
+//      4 WHEELER ROBOT WITH IT'S CUSTOM TRANSMITTER.
+//      THIS TEAM WORKS FOR ROBODARSHAN (ROBOTICS SOCIETY OF IIEST SHIBPUR) TO EXPLORE ROBOTICS DOMAIN.
+//      
+//      MEMBERS - AMIT BARMAN,SOPHOMORE AT IIEST SHIBPUR,ETC'25
+//                SOUVIK NATH,SOPHOMORE AT IIEST SHIBPUR,ME'25
+//      
+//      IN OUR PROJECT WE ARE GOING TO USE -
+//        1. AN ARDUINO UNO (FOR RECEIVER)
+//        2. AN ARDUINO NANO (FOR TRANSMITTER)
+//        3. 2 x NRF24LO1+PA+LNA MODULE
+//        4. 2 x JOYSTICKS
+//        5. 4 x BO MOTORS
+//        6. L298N MOTOR DRIVER
+//        7. BATTERY
+//      
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
