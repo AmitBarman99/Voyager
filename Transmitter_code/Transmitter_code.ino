4cca04d99d866d232c96c511a199073a9b6c3f3c
Amit Barman <amitbarrman15fifteen@gmail.com>
2022-11-13 03:42:01 +0530

    transmitter loop

--- Transmitter_code/Transmitter_code.ino
+++ Transmitter_code/Transmitter_code.ino
@@ -38,5 +38,8 @@ void setup() {
 }
 
 void loop() {
-
+  joystick[0]=analogRead(A4);    // A4 pin
+  joystick[1]=analogRead(A3);   // A3 pin
+  Serial.println(joystick[0]+','+joystick[1]);
+  radio.write(joystick,sizeof(joystick));
 }
