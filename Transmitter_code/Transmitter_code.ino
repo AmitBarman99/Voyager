eaf9df2fd2d99a7c2f35d2b926b9968495e258d3
Amit Barman <amitbarrman15fifteen@gmail.com>
2022-11-13 03:38:31 +0530

    Transmitter code setup

--- Transmitter_code/Transmitter_code.ino
+++ Transmitter_code/Transmitter_code.ino
@@ -20,13 +20,23 @@
 //    
 //       THIS PROGRAM WILL BE STORED IN TRANSMITTER'S ARDUINO.
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#include<nRF24L01.h>
+#include<RF24.h>
+#include<SPI.h>
+
+RF24 radio(9,10);   //CE and CSN pin
+const byte pipeaddress[20]="0001202002";    //same as receiver code
+char receiveData[100]="";
+int joystick[2];
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(pipeaddress);
+  radio.stopListening();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
